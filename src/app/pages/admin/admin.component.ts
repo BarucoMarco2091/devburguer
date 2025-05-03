ad00d936a73c5ac367532b57a0032dda5627cc2c
Marco <marcobaruco2091@gmail.com>
2025-05-03 16:07:07 -0300

    feat: login e admin

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  constructor(private auth: AuthService) { }
 
+  logout() {
+    this.auth.logout();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly isLoggedInKey = 'isLoggedIn';
+  constructor(private router: Router) { }
+  login(username: string, password: string): boolean {
+    if(username === 'admin' && password === '1234') {
+      localStorage.setItem(this.isLoggedInKey, 'true');
+      return true;
+    }
+    return false;
+  }
 
-  constructor() { }
+  logout(): void {
+    localStorage.removeItem(this.isLoggedInKey);
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem(this.isLoggedInKey) === 'true';
+  }
 }
