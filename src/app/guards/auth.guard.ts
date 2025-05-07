27a436e78b034abcbaa31404f488a94e516cd884
Marco <marcobaruco2091@gmail.com>
2025-05-07 16:18:25 -0300

    fim

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-  return isLoggedIn;
+  const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(platformId)) {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    return isLoggedIn || router.createUrlTree(['/login']); // Redireciona se não estiver logado
+  }
+  
+  return router.createUrlTree(['/login']); // Assume "não logado" no servidor
 };
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly isLoggedInKey = 'isLoggedIn';
-  constructor(private router: Router) { }
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) { }
   login(username: string, password: string): boolean {
-    if(username === 'admin' && password === '1234') {
-      localStorage.setItem(this.isLoggedInKey, 'true');
+    if (username === 'admin' && password === '1234') {
+
+      if (isPlatformBrowser(this.platformId)) {
+        localStorage.setItem(this.isLoggedInKey, 'true');
+      }
       return true;
     }
     return false;
   }
 
   logout(): void {
-    localStorage.removeItem(this.isLoggedInKey);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(this.isLoggedInKey);
+    }
     this.router.navigate(['/login']);
   }
 
   isLoggedIn(): boolean {
-    return localStorage.getItem(this.isLoggedInKey) === 'true';
+    if (isPlatformBrowser(this.platformId)) {
+      return localStorage.getItem(this.isLoggedInKey) === 'true';
+    }
+    return false;
   }
 }
